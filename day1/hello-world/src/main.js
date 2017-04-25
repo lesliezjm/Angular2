99a4f2ab519a28df69fe30f499997d2ec2b89668
Leslie <100113408@qq.com>
2017-04-25 23:36:48 +0800

    手动抄袭angular2 hello-world

---
+++ day1/hello-world/src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("core-js");
+require("zone.js");
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("./app.module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule)
+    .catch(function (err) { return console.error(err); });
+//# sourceMappingURL=main.js.map
